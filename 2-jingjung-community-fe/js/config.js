7e778b507020d344daf1bbdbfd49b6b4f9de4de1
jing-jung username <yhn5729@skuniv.ac.kr>
2026-03-20 10:05:13 +0900

    alb주소 수정

--- 2-jingjung-community-fe/js/config.js
+++ 2-jingjung-community-fe/js/config.js
@@ -1,3 +1,3 @@
 export const CONFIG = {
-    BASE_URL: "http://k8s-default-mainingr-ffc4d92d4c-1904231776.ap-southeast-2.elb.amazonaws.com/api"
+    BASE_URL: "http://k8s-default-mainingr-ffc4d92d4c-1797708319.ap-southeast-2.elb.amazonaws.com"
 };
