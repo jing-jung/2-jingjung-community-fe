29d1ce0bcedc02e4d2b32c666b78a3c54ee7c0ab
jing-jung username <yhn5729@skuniv.ac.kr>
2026-04-04 15:06:30 +0900

    주소 수정

--- 2-jingjung-community-fe/js/config.js
+++ 2-jingjung-community-fe/js/config.js
@@ -1,3 +1,3 @@
 export const CONFIG = {
-    BASE_URL: "http://k8s-myprojectgroup-d8b90bd933-1793738250.ap-southeast-2.elb.amazonaws.com/api"
+    BASE_URL: "http://k8s-myprojectgroup-d8b90bd933-873831510.ap-southeast-2.elb.amazonaws.com/api"
 };
