4ed935881b43f4559ad3df6c9db5c68b356d2896
jing-jung username <yhn5729@skuniv.ac.kr>
2026-04-05 13:50:39 +0900

    modify bell amount

--- 2-jingjung-community-fe/js/header.js
+++ 2-jingjung-community-fe/js/header.js
@@ -41,8 +41,8 @@ async function loadMyProfile() {
                 headerProfileIcon.style.backgroundColor = "transparent"; 
             }
 
-            if (bellAmountSpan && typeof user.bell !== 'undefined') {
-                bellAmountSpan.textContent = user.bell.toLocaleString();
+            if (bellAmountSpan && typeof user.bell_amount !== 'undefined') {
+                bellAmountSpan.textContent = user.bell_amount.toLocaleString();
             }
 
             const headerTitle = document.getElementById('headerTitle');
